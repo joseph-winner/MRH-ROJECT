dcf92df7d943d070d950ec889dd6e9ea1760122b
joseph-winner <muhanguzijoseph2021@gmail.com>
2024-11-21 20:36:15 +0300

    Switching to google maps

--- src/components/MapComponent/MapComponent.js
+++ src/components/MapComponent/MapComponent.js
@@ -1,27 +1,25 @@
 // MapComponent.js
 import React, { useEffect, useState } from 'react';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
-import L from 'leaflet';
-import Loader from '../Loader/Loader';
-import 'leaflet/dist/leaflet.css';
-
+import { GoogleMap, LoadScript, Marker, InfoWindow } from '@react-google-maps/api';
 
 const MapComponent = () => {
   // State to store the user's location
   const [location, setLocation] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
 
-  // Get user's location when the component mounts
+  // Your Google Maps API Key (replace with your own)
+  const googleMapsApiKey = "YOUR_GOOGLE_MAPS_API_KEY";
+
   useEffect(() => {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(
         (position) => {
           const { latitude, longitude } = position.coords;
-          // Store location in state
           setLocation({ lat: latitude, lng: longitude });
         },
         (error) => {
           console.error("Error getting location:", error);
-          alert("Could not retrieve your location");
+          alert("Could not retrieve your location.");
         }
       );
     } else {
@@ -29,26 +27,31 @@ const MapComponent = () => {
     }
   }, []);
 
-  // While location is being fetched, show a loading message
   if (!location) {
-    return <Loader />;
+    return <div>Loading map...</div>;
   }
 
   return (
-    <MapContainer center={location} zoom={13} style={{ height: "500px", width: "100%" }}>
-      {/* Tile Layer: OpenStreetMap */}
-      <TileLayer 
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      
-      {/* Marker: Show marker at the user's location */}
-      <Marker position={location}>
-        <Popup>
-          You are here!
-        </Popup>
-      </Marker>
-    </MapContainer>
+    <LoadScript googleMapsApiKey={googleMapsApiKey} onLoad={() => setIsLoaded(true)}>
+      {isLoaded && (
+        <GoogleMap
+          mapContainerStyle={{ height: '500px', width: '100%' }}
+          center={location}
+          zoom={14}
+        >
+          {/* Marker at the user's location */}
+          <Marker position={location} />
+          
+          {/* Optional: InfoWindow on the marker */}
+          <InfoWindow position={location}>
+            <div>
+              <h2>You are here!</h2>
+            </div>
+          </InfoWindow>
+        </GoogleMap>
+      )}
+    </LoadScript>
   );
-}
+};
 
 export default MapComponent;
